362940d48b90ba547d83371cb3c832f8830014d3
omer-friedman <34510256+omer-friedman@users.noreply.github.com>
2020-02-11 19:41:55 +0200

    add questions 16,17,18

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #define FALSE 0
 #define TRUE 1
+#define N 6
 
 typedef struct{
     char* first_name;
@@ -35,68 +36,84 @@ typedef struct{
 }hit;
 typedef hit* pHit;
 
+typedef struct{
+    char* word;
+    int index;
+    int direction;
+}crossword;
+
 int get_int_arr(int**);
-/*----------------------- Question 1: -----------------------*/
-void Question1();
+/*----------------------- question 1: -----------------------*/
+void question1();
 int print_digits(int);
-/*----------------------- Question 2: -----------------------*/
-void Question2();
+/*----------------------- question 2: -----------------------*/
+void question2();
 int num_sums(int[], int, int);
-/*----------------------- Question 3: -----------------------*/
-void Question3();
+/*----------------------- question 3: -----------------------*/
+void question3();
 int* bisearch(int[], int, int);
-/*----------------------- Question 4: -----------------------*/
-void Question4();
+/*----------------------- question 4: -----------------------*/
+void question4();
 int highest_digit(int);
-/*----------------------- Question 5: -----------------------*/
-void Question5();
+/*----------------------- question 5: -----------------------*/
+void question5();
 int what5(int*, int, int*);
-/*----------------------- Question 6: -----------------------*/
-void Question6();
+/*----------------------- question 6: -----------------------*/
+void question6();
 int what6_1(int*, int*);
 int what6_2(int*, int*, int);
-/*----------------------- Question 7: -----------------------*/
-void Question7();
+/*----------------------- question 7: -----------------------*/
+void question7();
 void my_itoa(int, char*);
 int get_size_of_num(int);
 char* person_to_string(person*);
 char* join_person(person*, int);
-/*----------------------- Question 8: -----------------------*/
-void Question8();
+/*----------------------- question 8: -----------------------*/
+void question8();
 pHit** makeMat(pHit*, int, int*, int*);
 pHit** updateMatSize(pHit**, int, int, int, int);
-/*----------------------- Question 9: -----------------------*/
-void Question9();
+/*----------------------- question 9: -----------------------*/
+void question9();
 float completeSquares(square*, int size);
 int isCompleteSquare(square);
 int getQuarter(point);
 float getDistance(point, point);
-/*----------------------- Question 10: -----------------------*/
-void Question10();
+/*----------------------- question 10: -----------------------*/
+void question10();
 char* str_insert(char*, char*, char*);
-/*----------------------- Question 11: -----------------------*/
-void Question11();
+/*----------------------- question 11: -----------------------*/
+void question11();
 int strremove(char*, const char*);
 int strremoveAll(char*, const char*);
-/*----------------------- Question 12: -----------------------*/
-void Question12();
+/*----------------------- question 12: -----------------------*/
+void question12();
 char* moveWords(char*, int*);
 char** getWordsArray(char*);
 int getWordSize(char*);
 int cntWords(char*);
-/*----------------------- Question 13: -----------------------*/
-void Question13();
+/*----------------------- question 13: -----------------------*/
+void question13();
 void subSentence(char*, int);
 void setNoSpacesSent(char*, char*);
-/*----------------------- Question 14: -----------------------*/
-void Question14();
+/*----------------------- question 14: -----------------------*/
+void question14();
 int gasoline(FILE**, int, char*);
 int get_min_car_index(car_gas*, int);
 void update_working_car(car_gas*, car_gas*);
 int read_next_car(FILE*, car_gas*);
-/*----------------------- Question 15: -----------------------*/
-void Question15();
+/*----------------------- question 15: -----------------------*/
+void question15();
 int publicSale(FILE*, FILE*, char*, char*);
+/*----------------------- question 16: -----------------------*/
+void question16();
+void path(int, int[N][N]);
+void recsolve (int, int[N][N], int[N][N], int, int, int*, int);
+/*----------------------- question 17: -----------------------*/
+void question17();
+int printStrings(int, int, char*, int, int);
+/*----------------------- question 17: -----------------------*/
+void question18();
+void mergeStrings(char*, char*, char*);
 
 
 void main() {
@@ -106,49 +123,58 @@ void main() {
         scanf("%d%*c", &action);
         switch(action){
             case 1:
-                Question1();
+                question1();
                 break;
             case 2:
-                Question2();
+                question2();
                 break;
             case 3:
-                Question3();
+                question3();
                 break;
             case 4:
-                Question4();
+                question4();
                 break;
             case 5:
-                Question5();
+                question5();
                 break;
             case 6:
-                Question6();
+                question6();
                 break;
             case 7:
-                Question7();
+                question7();
                 break;
             case 8:
-                Question8();
+                question8();
                 break;
             case 9:
-                Question9();
+                question9();
                 break;
             case 10:
-                Question10();
+                question10();
                 break;
             case 11:
-                Question11();
+                question11();
                 break;
             case 12:
-                Question12();
+                question12();
                 break;
             case 13:
-                Question13();
+                question13();
                 break;
             case 14:
-                Question14();
+                question14();
                 break;
             case 15:
-                Question15();
+                question15();
+                break;
+            case 16:
+                question16();
+                break;
+            case 17:
+                question17();
+                break;
+            case 18:
+                question18();
                 break;
             default:
                 printf("Wrong choice.\n");
@@ -156,12 +182,23 @@ void main() {
     }while(action != 0);
 }
 
-void Question1(){
+void question1(){
     int num;
     printf("Enter num:");
     scanf("%d%*c", &num);
     num = highest_digit(num);
     printf("Highest digit: %d\n", num);
+}
+
+int highest_digit(int num){
+    int sheerit, rest_max;
+    if(num == 0)
+        return 0;
+    sheerit = num%10;
+    rest_max = highest_digit(num/10);
+    if(sheerit > rest_max)
+        return sheerit;
+    return rest_max;
 }
 
 int get_int_arr(int** arr){
@@ -184,28 +221,12 @@ int get_int_arr(int** arr){
     return arr_size;
 }
 
-void Question4(){
-    int arr_size, num, *arr=NULL, *p=NULL;
-    arr_size = get_int_arr(&arr);
-    printf("Enter num to search:");
-    scanf("%d", &num);
-    p = bisearch(arr, arr_size, num);
-    if(p)
-        printf("%d Found In Address %d -> index %d in array\n", num, (int)p, ((int)p-(int)arr)/4);
-    else
-        printf("%d Not Found.\n", num);
-    free(arr);
-}
-
-int highest_digit(int num){
-    int sheerit, rest_max;
-    if(num == 0)
-        return 0;
-    sheerit = num%10;
-    rest_max = highest_digit(num/10);
-    if(sheerit > rest_max)
-        return sheerit;
-    return rest_max;
+void question2(){
+    int num;
+    printf("Enter num:");
+    scanf("%d%*c", &num);
+    num = print_digits(num);
+    printf("\n Sum:%d\n", num);
 }
 
 int print_digits(int num) {
@@ -220,26 +241,15 @@ int print_digits(int num) {
     return sheerit + rest_sum;
 }
 
-void Question2(){
-    int num;
-    printf("Enter num:");
-    scanf("%d%*c", &num);
-    num = print_digits(num);
-    printf("\n Sum:%d\n", num);
+void question3(){
+    int *arr, arr_size, num;
+    arr_size = get_int_arr(&arr);
+    printf("Enter Sum:");
+    scanf("%d", &num);
+    num = num_sums(arr, arr_size, num);
+    printf("Total Sums:%d\n", num);
+    free(arr);
 }
-
-//int num_sums(int* a, int size, int n){
-//    int i, cnt_cur=0;
-//    if(size == 0)
-//        return 0;
-//    for(i=0; i<size; i++){
-//        if(n-a[i] == 0)
-//            cnt_cur++;
-//        else if(n-a[i] > 0)
-//            cnt_cur +=num_sums(a+i+1, size-i-1, n-a[i]);
-//    }
-//    return cnt_cur;
-//}
 
 int num_sums(int *a, int size, int n) {
     if (size<=0) {
@@ -251,15 +261,19 @@ int num_sums(int *a, int size, int n) {
     return *a == n ? 1 : num_sums(a+1, size-1, n-*a) + num_sums(a+1, size-1, n);
 }
 
-void Question3(){
-    int *arr, arr_size, num;
+void question4(){
+    int arr_size, num, *arr=NULL, *p=NULL;
     arr_size = get_int_arr(&arr);
-    printf("Enter Sum:");
+    printf("Enter num to search:");
     scanf("%d", &num);
-    num = num_sums(arr, arr_size, num);
-    printf("Total Sums:%d\n", num);
+    p = bisearch(arr, arr_size, num);
+    if(p)
+        printf("%d Found In Address %d -> index %d in array\n", num, (int)p, ((int)p-(int)arr)/4);
+    else
+        printf("%d Not Found.\n", num);
     free(arr);
 }
+
 int* bisearch(int* arr, int size, int num) {
     int cur_num;
     if (size <= 0)
@@ -272,7 +286,7 @@ int* bisearch(int* arr, int size, int num) {
     return bisearch(arr+(size/2)+1, size%2 == 0? (size/2)-1:(size/2), num);
 }
 
-void Question5(){
+void question5(){
     int result, num;
     int a1[] = {3, 4, 3, 8};
     int a2[] = {3, 8, 9, 3, 2, 5, 2, 7, 2, 9};
@@ -301,7 +315,7 @@ int what5(int *arr, int size, int *p){
     return number;
 }
 
-void Question6() {
+void question6() {
     int arr1[] = {1, 2, 3, 4, -1};
     int arr2[] = {5, 6, 7, 8, -1};
     printf("%d\n", what6_1(arr1,arr2));
@@ -324,7 +338,7 @@ int what6_2(int* arr1, int*arr2, int i){
         return *arr2 - what6_2(arr1+1, arr2+1, 1-i);
 }
 
-void Question7(){
+void question7(){
     person persons[3];
     persons[0].first_name = (char*)malloc(sizeof(char)*20);
     persons[0].last_name = (char*)malloc(sizeof(char)*20);
@@ -397,7 +411,7 @@ char* join_person(person* per_arr, int size){
     return new_per_str;
 }
 
-void Question8(){
+void question8(){
     int plines, pcolumns, arr_size = 5;
     int i, j;
     pHit arr[arr_size];
@@ -461,7 +475,7 @@ pHit** updateMatSize(pHit** oldMat, int old_row, int old_col, int new_row, int n
     return newMat;
 }
 
-void Question9(){
+void question9(){
     square squares[3];
     //square[0] is a complete square!
     squares[0].pointArr[0].x = 50;  squares[0].pointArr[0].y = 50;
@@ -520,7 +534,7 @@ int getQuarter(point p){
     return -1;
 }
 
-void Question10(){
+void question10(){
     char* str = "I like ice cream";
     char* new_str = str_insert(str, "do not ", str+2);
     printf("%s\n", new_str);
@@ -550,7 +564,7 @@ char* str_insert(char* str1, char* str2, char* at){
     return new_str;
 }
 
-void Question11(){
+void question11(){
     char str[] = "The bigger the error the lower the grade.";
     int cnt_found = strremoveAll(str, "er");
     puts(str);
@@ -597,7 +611,7 @@ int strremoveAll(char* str, const char* rem){
     return cnt;
 }
 
-void Question12(){
+void question12(){
     char *new_str = NULL;
     char str[] = "You have         to dream before your dreams can come true";
     int arr[] = {1, 8, 13, 4, 15, 5, 6, 4, 9, 18, 10, 17, -1};
@@ -669,7 +683,7 @@ int getWordSize(char* sentence){
     return cnt_chars;
 }
 
-void Question13(){
+void question13(){
     char sent[] = "This is a sentence";
     subSentence(sent,2);
 }
@@ -699,15 +713,15 @@ void setNoSpacesSent(char* sent, char* sentNoSpaces){
     sentNoSpaces[cnt_relevant] = '\0';
 }
 
-void Question14(){
+void question14(){
     int size = 3;
     FILE* arr[size];
     char cwd[PATH_MAX];
     getcwd(cwd, sizeof(cwd));
-    if(!(arr[0] = fopen("Question14\\gasoline_in1.txt", "r"))) exit(1);
-    if(!(arr[1] = fopen("Question14\\gasoline_in2.txt", "r"))) exit(1);
-    if(!(arr[2] = fopen("Question14\\gasoline_in3.txt", "r"))) exit(1);
-    if(gasoline(arr, size, "Question14\\gasoline_out.txt"))
+    if(!(arr[0] = fopen("question14\\gasoline_in1.txt", "r"))) exit(1);
+    if(!(arr[1] = fopen("question14\\gasoline_in2.txt", "r"))) exit(1);
+    if(!(arr[2] = fopen("question14\\gasoline_in3.txt", "r"))) exit(1);
+    if(gasoline(arr, size, "question14\\gasoline_out.txt"))
         printf("Done! check %s\n", cwd);
     else
         printf("Something went wrong :(\n");
@@ -787,10 +801,10 @@ int read_next_car(FILE* file, car_gas* next_car){
     return TRUE;
 }
 
-void Question15(){
-    FILE *forSale = fopen("Question15\\items.txt", "r");
-    FILE *offers = fopen("Question15\\offers.txt", "r");
-    publicSale(forSale, offers,"Question15\\notSold.txt", "Question15\\sold.txt");
+void question15(){
+    FILE *forSale = fopen("question15\\items.txt", "r");
+    FILE *offers = fopen("question15\\offers.txt", "r");
+    publicSale(forSale, offers,"question15\\notSold.txt", "question15\\sold.txt");
     fclose(forSale);
     fclose(offers);
 }
@@ -822,4 +836,96 @@ int publicSale(FILE *forSale, FILE *offers, char * notSold, char* sold) {
     if (fclose(fNotSold) == EOF || fclose(fSold) == EOF)
         return -1;
     return total;
+}
+
+void question16(){
+    int board[N][N] ={{1,1,1,1,1,1},
+                      {2,3,4,5,1,1},
+                      {2,2,1,1,1,1},
+                      {3,7,1,7,6,1},
+                      {4,6,1,1,1,1},
+                      {5,8,7,8,4,1}
+    };
+    path(N, board);
+}
+
+void recsolve (int size, int board[N][N], int visit[N][N], int r, int c, int* shortestLength, int lengthTillNow){
+    visit[r][c]=1;
+    if ( (r == size-1) && (c == size-1) ) {
+        if ( *shortestLength > lengthTillNow )
+            *shortestLength = lengthTillNow ;
+    }
+    if ( r>0 && board[r-1][c]==board[0][0] && !visit[r-1][c] && *shortestLength >lengthTillNow)
+        recsolve(size, board, visit, r-1, c, shortestLength, lengthTillNow+1);
+    if ( r < size-1 && board[r+1][c]==board[0][0] &&visit[r+1][c] == 0 && *shortestLength > lengthTillNow)
+        recsolve(size, board, visit, r+1, c, shortestLength, lengthTillNow+1);
+    if ( c>0 && board[r][c-1] == board[0][0] && visit[r][c-1] ==0 &&*shortestLength > lengthTillNow)
+        recsolve(size, board, visit, r, c-1, shortestLength, lengthTillNow+1);
+    if (c < size-1 && board[r][c+1]==board[0][0] && visit[r][c+1]==0&& *shortestLength > lengthTillNow)
+        recsolve(size, board, visit, r, c+1, shortestLength, lengthTillNow+1);
+    visit[r][c] = 0;
+}
+
+void path (int size, int board[N][N]){
+    int visit[N][N] = {0};
+    int shortestLength = size*size;
+    if (board[0][0]!=board[size-1][size-1])
+        printf("0\n");
+    else{
+        recsolve(size, board,visit,0,0, &shortestLength, 1 );
+        if (shortestLength!=size*size)
+            printf("%d\n",shortestLength);
+        else
+            printf("0\n");
+    }
+    return;
+}
+
+void question17(){
+    char str[4];
+    str[4] = '\0';
+    printf("printStrings: %d\n", printStrings(0, 3, str, 0, 0));
+}
+
+int printStrings(int i, int n, char* str, int a, int c){
+    int count = 0;
+    if(i==n){
+        printf("%s, ", str);
+        return 1;
+    }
+    if(a==1)
+        a = 0;
+    else{
+        str[i] = 'a';
+        count+=printStrings(i+1, n, str, a+1, c);
+    }
+    str[i] = 'b';
+    count += printStrings(i+1, n, str, a, c);
+    if(c==2)
+        c = 1;
+    else{
+        str[i] = 'c';
+        count += printStrings(i+1, n, str, a, c+1);
+    }
+    return count;
+}
+
+void question18(){
+    char s1[] = "abcf";
+    char s2[] = "ade";
+    char s3[strlen(s1)+strlen(s2)];
+    s3[strlen(s1)+strlen(s2)] = '\0';
+    mergeStrings(s1, s2, s3);
+    printf("First String: %s\nSecond String: %s\nMerged String: %s\n", s1, s2, s3);
+}
+
+void mergeStrings(char* str1, char* str2, char* str3){
+    if(!*str1 && !*str2)
+        return;
+    if(!*str1 || *str2<*str1){
+        *str3 = *str2;
+        return mergeStrings(str1, str2+1, str3+1);
+    }
+    *str3 = *str1;
+    return mergeStrings(str1+1, str2, str3+1);
 }
